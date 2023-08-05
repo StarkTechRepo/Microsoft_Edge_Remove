a5b02d66a10206a22cd65332f3fcaf3b318bd948
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-05 18:21:31 -0300

    Update remover_edge.bat

--- remover_edge.bat
+++ remover_edge.bat
@@ -1,40 +1,67 @@
 @echo off
 title - https://github.com/OtaconEvil
 
-:: Comprobar si se está ejecutando con permisos de administrador
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Este script requiere permisos de administrador para funcionar correctamente.
-    echo Por favor, ejecuta el script como administrador.
-    pause
-    exit /b 1
+REM Script para mejorar la privacidad y desinstalar Microsoft Edge Chromium
+
+REM Comprobar si se tienen privilegios de administrador
+fltmc >nul 2>&1 || (
+    echo Privilegios de administrador requeridos.
+    PowerShell Start -Verb RunAs "%0" 2>nul || (
+        echo Haz clic derecho en el script y selecciona "Ejecutar como administrador".
+        pause & exit 1
+    )
+    exit 0
 )
 
-:: Función para desinstalar Microsoft Edge
-:desinstalar_edge
-echo Desinstalando Microsoft Edge...
-start /wait "" cmd /c "C:\Windows\System32\cmd.exe" /c "C:\Program Files (x86)\Microsoft\Edge\Application\setup.exe" --uninstall --system-level --force-uninstall
-timeout 2 >nul
-exit /b
+REM Función para actualizar el registro con un valor DWORD en una ruta específica
+:ActualizarRegistro
+reg add "%~1" /v "%~2" /t REG_DWORD /d %~3 /f
+goto :eof
 
-:: Función para desinstalar WebView de Edge
-:desinstalar_webview
-echo Desinstalando WebView de Edge...
-start /wait "" cmd /c "C:\Windows\System32\cmd.exe" /c "C:\Program Files (x86)\Microsoft\EdgeWebView\Application\setup.exe" --uninstall --msedgewebview --system-level --force-uninstall
-timeout 2 >nul
-exit /b
+REM ----------------------------------------------------------
+REM -------------------Desactivar seguimiento------------------
+REM ----------------------------------------------------------
+echo --- Desactivar seguimiento
+call :ActualizarRegistro "HKCU\Software\Policies\Microsoft\Windows\EdgeUI" "DisableMFUTracking" 1
+call :ActualizarRegistro "HKCU\Software\Policies\Microsoft\MicrosoftEdge\Main" "PreventLiveTileDataCollection" 1
+call :ActualizarRegistro "HKCU\Software\Policies\Microsoft\Windows\EdgeUI" "DisableRecentApps" 1
+call :ActualizarRegistro "HKCU\Software\Policies\Microsoft\Windows\EdgeUI" "TurnOffBackstack" 1
+goto :eof
 
-:: Desinstalar Microsoft Edge y WebView
-if exist "C:\Program Files (x86)\Microsoft\Edge\Application" (
-    call :desinstalar_edge
-) else (
-    echo Microsoft Edge no encontrado.
-)
+REM ----------------------------------------------------------
+REM --------Desactivar sugerencias de búsqueda en Edge--------
+REM ----------------------------------------------------------
+echo --- Desactivar sugerencias de búsqueda en Edge
+call :ActualizarRegistro "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\SearchScopes" "ShowSearchSuggestionsGlobal" 0
+goto :eof
 
-if exist "C:\Program Files (x86)\Microsoft\EdgeWebView\Application" (
-    call :desinstalar_webview
-) else (
-    echo WebView de Edge no encontrado.
-)
+REM Desactivar la recopilación de datos relacionados con el uso y los bloqueos de Edge
+echo --- Desactivar la recopilación de datos relacionados con el uso y los bloqueos de Edge
+call :ActualizarRegistro "HKLM\SOFTWARE\Policies\Microsoft\Edge" "MetricsReportingEnabled" 0
+call :ActualizarRegistro "HKLM\SOFTWARE\Policies\Microsoft\Edge" "SendSiteInfoToImproveServices" 0
+goto :eof
+
+REM ----------------------------------------------------------
+REM ------Desactivar instalación automática de Edge----------
+REM ----------------------------------------------------------
+echo --- Desactivar instalación automática de Edge
+call :ActualizarRegistro "HKLM\SOFTWARE\Microsoft\EdgeUpdate" "DoNotUpdateToEdgeWithChromium" 1
+goto :eof
+
+REM Desactivar SmartScreen en Edge
+echo --- Desactivar SmartScreen en Edge
+call :ActualizarRegistro "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" "EnabledV9" 0
+call :ActualizarRegistro "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" "PreventOverride" 0
+call :ActualizarRegistro "HKCU\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" "EnabledV9" 0
+call :ActualizarRegistro "HKCU\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" "PreventOverride" 0
+goto :eof
+
+REM ----------------------------------------------------------
+REM ----------------------Desinstalar Edge---------------------
+REM ----------------------------------------------------------
+echo --- Desinstalar Edge (chromium-based)
+PowerShell -ExecutionPolicy Unrestricted -Command "$installer = (Get-ChildItem '^"%ProgramFiles%*\Microsoft\Edge\Application\*\Installer\setup.exe'^'); if (!$installer) { Write-Host 'No se pudo encontrar el instalador'; } else { & $installer.FullName -Uninstall -System-Level -Verbose-Logging -Force-Uninstall; }"
+goto :eof
 
 pause
+
